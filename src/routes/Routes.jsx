978476f4a38f9161e976e56f1d60bcc27f57e377
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-04-28 23:13:39 +0600

    news data fetched

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,19 +3,32 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home/Home";
 import Main from "../Layout/Main";
 import Category from "../Pages/Home/Category/Category";
+import Newslayout from "../Layout/Newslayout";
+import News from "../Pages/News/News";
 
 const router = createBrowserRouter([
     {
         path : '/',
         element: <Main></Main>,
-        Children: [
+        children: [
             {
                 path: '/',
                 element: <Home></Home>
             },
             {
                 path: '/category/:id',
-                element: <Category></Category>
+                element: <Category></Category>,
+                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+            }  
+        ]
+    },
+    {
+        path: '/news',
+        element: <Newslayout></Newslayout>,
+        children: [
+            {
+                path: '/news/:id',
+                element: <News></News>
             }
         ]
     }
