300a434b00c12dd85b7bd91e2f5bb265ab5cd87d
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-04-30 10:30:09 +0600

    hide firebase auth key

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('environment variable', import.meta.env.VITE_APIKEY)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAd4noXQE1C7-VvpIba8a73Zz2UN7vmGeo",
-  authDomain: "the-news-dragon-24548.firebaseapp.com",
-  projectId: "the-news-dragon-24548",
-  storageBucket: "the-news-dragon-24548.appspot.com",
-  messagingSenderId: "1070790758877",
-  appId: "1:1070790758877:web:5b9503e91a2a24563d2634"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
